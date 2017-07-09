2d99d376de024a8ab7fb97a80da81059aa2f4acd
ZombieArg <white_zombie_85@hotmail.com>
2017-07-09 17:12:57 -0300

    Microscope app - client template - helpers - styles

---
+++ microscope/client/templates/posts/post_item.js
@@ -0,0 +1,10 @@
+/**
+ * Created by eduardopan on 7/9/17.
+ */
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+    }
+});
