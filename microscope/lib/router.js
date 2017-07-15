78e83ac49f3591d72331700f54d73814651a628c
ZombieArg <white_zombie_85@hotmail.com>
2017-07-14 21:07:45 -0300

    Microscope app - waiting template

--- microscope/lib/router.js
+++ microscope/lib/router.js
@@ -2,7 +2,9 @@
  * Created by eduardopan on 7/14/17.
  */
 Router.configure({
-    layoutTemplate: 'layout'
+    layoutTemplate: 'layout',
+    loadingTemplate: 'loading',
+    waitOn: function(){ return Meteor.subscribe('posts');}
 });
 
 Router.route('/', {name:'postsList'});
