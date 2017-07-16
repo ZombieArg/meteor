e8a3b5b51eede9eeb875e3e2bd2e64bf2b23cb8f
ZombieArg <white_zombie_85@hotmail.com>
2017-07-16 12:46:27 -0300

    Microscope app - remove insecure - allow posts from logged users

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -3,3 +3,10 @@
  */
 
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+    insert: function (userId, doc) {
+        //Only allow posting if you are logged in
+        return !! userId;
+    }
+});
