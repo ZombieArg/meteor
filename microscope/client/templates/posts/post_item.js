6bc9fe02f8e63924620020ff414c323c43454b05
ZombieArg <white_zombie_85@hotmail.com>
2017-07-19 18:46:28 -0300

    Microscope app - Form edition

--- microscope/client/templates/posts/post_item.js
+++ microscope/client/templates/posts/post_item.js
@@ -2,6 +2,9 @@
  * Created by eduardopan on 7/9/17.
  */
 Template.postItem.helpers({
+    ownPost: function () {
+      return this.userId === Meteor.userId();
+    },
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
