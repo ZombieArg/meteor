c7c39fc01c5bf2e562dcf62fa7d2a57871ecb18d
ZombieArg <white_zombie_85@hotmail.com>
2017-07-19 19:02:55 -0300

    Microscope app - permissions to check owner of the post

---
+++ microscope/lib/permissions.js
@@ -0,0 +1,9 @@
+/**
+ * Created by eduardopan on 7/19/17.
+ */
+
+// check that the userId specified owns the documents
+ownsDocument = function (userId, doc) {
+    return doc && doc.userId === userId;
+    
+}
