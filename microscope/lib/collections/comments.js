995060ad95b04b5dbed1cb57ddd604c76017b7d1
ZombieArg <white_zombie_85@hotmail.com>
2017-07-25 23:22:11 -0300

    Microscope app - updated meteor from 1.5 to 1.5.1 - Comments collection, publish/subscribe and test data

---
+++ microscope/lib/collections/comments.js
@@ -0,0 +1,5 @@
+/**
+ * Created by eduardopan on 7/25/17.
+ */
+
+Comments = new Mongo.Collection('comments');
--- microscope/server/publications.js
+++ microscope/server/publications.js
@@ -1,6 +1,11 @@
 /**
  * Created by eduardopan on 7/9/17.
  */
+
 Meteor.publish('posts', function () {
    return Posts.find();
+});
+
+Meteor.publish('comments', function () {
+    return Comments.find();
 });
